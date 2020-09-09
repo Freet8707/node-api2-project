c6f5d7d8fa6ed1ccc83f21e8c5ab41e0d97ec4b6
Freet8707 <freet8707@gmail.com>
2020-09-08 20:53:10 -0500

    all get requests are running successfully

--- routes/expressRouter.js
+++ routes/expressRouter.js
@@ -16,6 +16,29 @@ router.get("/", (req, res) => {
     })
 })
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((error) => {
+            res.status(400).json({ message: "there was an error" })
+        })
+})
+
+router.get("/:id/comments", (req, res) => {
+
+    db.findPostComments(req.params.id)
+        .then((comments) => {
+            res.status(200).json(comments)
+        })
+        .catch((error) => {
+            res.status(400).json({ message: "there was an error" })
+        })
+})
+
 router.post("/", (req, res) => {
 
     db.insert(req.body)
@@ -28,5 +51,28 @@ router.post("/", (req, res) => {
             res.status(500).json({ message: "there was an error" });
         })
 })
+
+router.post("/:id/comments", (req, res) => {
+    // db.findById(req.params.id)
+    //     .then((post) => {
+    //         console.log(post);
+    //         res.status(200).json(post);
+    //     })
+    //     .catch((error) => {
+    //         res.status(500).json(error);
+    //     })
+    // const body = { ...req.body, id: req.params.id }
+
+    db.insertComment(req.body)
+        .then((comment) => {
+            console.log(comment)
+            res.status(201).json(comment)
+        })
+        .catch((err) => {
+            res.status(500).json({ message: "there was an error"})
+        })
+})
+
+
 
 module.exports = router
